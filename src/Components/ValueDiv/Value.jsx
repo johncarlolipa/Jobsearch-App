33f7302811c5c0a69acb64717e5681a603f45335
JOHN CARLO LIPA <johncarlolipa@gmail.com>
2023-03-19 16:41:16 +0800

    footer added

--- src/Components/ValueDiv/Value.jsx
+++ src/Components/ValueDiv/Value.jsx
@@ -43,6 +43,16 @@ const Value = () => {
             </div>
         </div>
 
+        <div className="card mt-[2rem] flex justify-between bg-blueColor p-[5rem] rounded-[10px]">
+
+            <div>
+                <h1 className='text-blueColor text-[30px] font-bold'>Ready to switch a career?</h1>
+                <h2 className='text-textColor text-[25px] font-bold'>Let's get Started</h2>
+            </div>
+                <button className='border-[2px] rounded-[10px] py-[4px] px-[50px] text-[18px] font-semibold text-textColor hover:bg-blueColor border-blueColor'>Get Started</button>
+
+        </div>
+
     </div>
   )
 }
